0e4badcc4bbcfc921af848736862da31bb6faa0f
Jack <jwleggett@outlook.com>
2023-05-22 17:27:59 +0100

    test for bank account contructing as an empty account passes

---
+++ bank.js
@@ -0,0 +1,12 @@
+class BankAccount {
+
+  constructor() {
+    this.amount = 0;
+  }
+
+  getAmount() {
+    return this.amount
+  }
+}
+
+module.exports = BankAccount
--- bank.test.js
+++ bank.test.js
@@ -1,5 +1,11 @@
-const Bank = require('./bank')
+const BankAccount = require('./bank')
 
 describe('Bank class', () => {
   
+  it('constructs as an empty bank account', () => {
+    const account = new BankAccount();
+    let result = account.getAmount();
+    expect(result).toEqual(0)
+
+  })
 })
