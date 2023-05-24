fad21dc450a53f951c889ea322e9fd36225bdd1f
Jack <jwleggett@outlook.com>
2023-05-24 10:31:32 +0100

    left the print function inside of account

--- statement.test.js
+++
@@ -1,9 +0,0 @@
-const Statement = require('./statement')
-
-describe('statement class', () => {
-
-  it('prints an empty statement with just the header', () => {
-    const statement = new Statement('');
-    expect(statement.print()).toEqual('date || credit || debit || balance')
-  })
-})
